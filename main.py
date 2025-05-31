c3deab9f93465a72f5da640bcab75d2f89196766
Egor <paradize9140@gmail.com>
2025-05-31 10:08:07 +0200

    Initial FastAPI app for Railway

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get("/ping")
-def ping():
-    return {"status": "online"}
+@app.post("/stoplist")
+async def receive_stoplist(request: Request):
+    data = await request.json()
+    print("🔔 Получен POST-запрос от iiko:")
+    print(data)
+    return {"status": "ok"}
