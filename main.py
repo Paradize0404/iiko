efe02f5c6efbbe30567896f0c11dba3b3821223e
Egor <paradize9140@gmail.com>
2025-05-31 10:20:26 +0200

    Initial FastAPI app for Railway

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI, Request
-import os
-import uvicorn
-
 
 app = FastAPI()
 
@@ -11,11 +8,3 @@ async def receive_stoplist(request: Request):
     print("🔔 Получен POST-запрос от iiko:")
     print(data)
     return {"status": "ok"}
-
-
-
-
-# 👇 Добавь вот это в самый конец
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
